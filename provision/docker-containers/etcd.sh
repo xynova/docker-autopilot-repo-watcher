62841702d8f44a5de5ae3b1795a34adc414ffc58
hec <me@xynova.com>
2016-04-16 03:59:56 +0000

    moved ETCD data-dir and wal-dir to a volume

--- provision/docker-containers/etcd.sh
+++ provision/docker-containers/etcd.sh
@@ -1,9 +1,23 @@
 #! /bin/bash
 
+DATA_DIR=/var/lib/etcd/data
+WRITE_AHEAD_LOG_DIR=/var/lib/etcd/wal
+ETCD_UID=$(id etcd -u)
+ETCD_GID=$(id etcd -g)
+
+mkdir -p $DATA_DIR $WRITE_AHEAD_LOG_DIR
+chown $ETCD_UID:$ETCD_GID $DATA_DIR $WRITE_AHEAD_LOG_DIR
+
 HOST_IP=$(ip addr | grep 'state UP' -A2 | grep eth0 | tail -1 | awk '{print $2}' | cut -f1  -d'/') \
-&& docker run -d --restart unless-stopped --hostname ${HOSTNAME} \
-	--name etcd -p 2379:2379 -p 2380 -e HOST_IP=${HOST_IP} \
-	 quay.io/coreos/etcd:latest \
+&& docker run -d --restart unless-stopped \
+	--hostname ${HOSTNAME} \
+	--name etcd -p 2379:2379 \
+	-p 2380 \
+	-e HOST_IP=${HOST_IP} \
+	-v $DATA_DIR:$DATA_DIR:Z \
+	-v $WRITE_AHEAD_LOG_DIR:$WRITE_AHEAD_LOG_DIR:Z \
+	-u $ETCD_UID:$ETCD_GID \
+	quay.io/coreos/etcd:latest \
 	-advertise-client-urls http://${HOST_IP}:2379,http://127.0.0.1:2379 \
 	-listen-client-urls http://0.0.0.0:2379 \
 	-initial-advertise-peer-urls http://${HOST_IP}:2380 \
@@ -11,6 +25,8 @@ HOST_IP=$(ip addr | grep 'state UP' -A2 | grep eth0 | tail -1 | awk '{print $2}'
 	-initial-cluster-token etcd-cluster-1 \
 	-name=${HOSTNAME} \
 	-initial-cluster ${HOSTNAME}=http://${HOST_IP}:2380 \
-	-initial-cluster-state new
+	-initial-cluster-state new \
+	-data-dir $DATA_DIR \
+	-wal-dir $WRITE_AHEAD_LOG_DIR
 
 
