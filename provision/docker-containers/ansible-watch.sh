52e81b2b6da3390d733c63d36bdeb29852547cb9
hec <me@xynova.com>
2016-04-20 15:20:17 +0000

    adding selinux volume z flags to ansible containers

--- provision/docker-containers/ansible-watch.sh
+++ provision/docker-containers/ansible-watch.sh
@@ -13,13 +13,13 @@ ANSIBLE_VOL=$WORK_DIR/ansible-watchrepo
 CONFIG_VOL=$WORK_DIR/dockerdata
 REPO_VOL=$WORK_DIR/gitrepos/${GIT_WATCHED_REPO_BRANCH}
 
-docker run --restart unless-stopped -d \
+docker run -t --restart unless-stopped -d \
         `#-v $(which docker):/usr/bin/docker:ro` \
         `#-v $(ldconfig -p | grep 'libdevmapper.so.1.02' | awk '{print $4}'):/usr/lib/libdevmapper.so.1.02:ro` \
         `#--security-opt label:disable` \
-        -v $REPO_VOL:$REPO_VOL \
-        -v $CONFIG_VOL:$CONFIG_VOL \
-        -v $ANSIBLE_VOL:/home/ansible \
+        -v $REPO_VOL:$REPO_VOL:z \
+        -v $CONFIG_VOL:$CONFIG_VOL:z \
+        -v $ANSIBLE_VOL:/home/ansible:z \
 	-e DEPLOYMENT_GROUP=$DEPLOYMENT_GROUP \
         -e GITREPOS_DIR=$REPO_VOL \
         -e GIT_WATCHED_REPO_BRANCH=$GIT_WATCHED_REPO_BRANCH \
--- provision/docker-containers/ansible.sh
+++ provision/docker-containers/ansible.sh
@@ -17,9 +17,9 @@ docker run -it --rm \
 	`#-v $(which docker):/usr/bin/docker:ro` \
 	`#-v $(ldconfig -p | grep 'libdevmapper.so.1.02' | awk '{print $4}'):/usr/lib/libdevmapper.so.1.02:ro` \
 	`# --security-opt label:disable `\
-	-v $REPO_VOL:$REPO_VOL \
-	-v $CONFIG_VOL:$CONFIG_VOL \
-	-v $ANSIBLE_VOL:/home/ansible \
+	-v $REPO_VOL:$REPO_VOL:z \
+	-v $CONFIG_VOL:$CONFIG_VOL:z \
+	-v $ANSIBLE_VOL:/home/ansible:z \
 	-e DEPLOYMENT_GROUP=$DEPLOYMENT_GROUP \
 	-e GITREPOS_DIR=$REPO_VOL \
 	-e GIT_WATCHED_REPO_BRANCH=$GIT_WATCHED_REPO_BRANCH \
